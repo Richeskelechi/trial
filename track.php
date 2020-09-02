2990ebbcddeb67ed5cffd4395a9cdf8c2f16d564
Richeskelechi <richeskelechi94.rk@gmail.com>
2020-09-02 12:16:35 +0100

    updated successfully

---
+++ track.php
@@ -0,0 +1,413 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vinns Courier Services</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Transload a Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href="css/simpleLightbox.css" rel='stylesheet' type='text/css' />
+	<link href="css/popup-box.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+</head>
+
+<body>
+<header>
+	<div class="header">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ <a class="navbar-brand" href="index.html">Vinns C<span><i class="fab fa-empire"></i></span>urier</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="track.php">Track<span class="sr-only">(current)</span></a>
+      </li>
+	<form action="trackgood.php" method="post">
+		<input type="text" name="good" style="background-color: transparent;border-radius: none; border: 1px solid black; color: white" placeholder="Enter Tracking Number" required="">
+		<input type="submit" name="track" class="bg-success" style="border: none; cursor: pointer;" value="Click To Track">
+	</form>
+    </ul>
+    <div class="navbar-text">
+     <p class="right-p"><i class="fas fa-phone" aria-hidden="true"></i>7085733574</p>
+    </div>
+  </div>
+</nav>
+		</div>
+</header>
+<!--/banner-->
+	<section class="banner-top">
+	<div class="banner">
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+			</ol>
+			<div class="carousel-inner" role="listbox">
+				<div class="carousel-item active">
+					<div class="carousel-caption">
+							<h3>Transport your cargo everywhere</h3>
+						<p>Vinns Courier Services offer a diverse range of transportation services from project cargo to international transportation and domestic retail distribution and delivery.</p>
+								<a href="#" data-toggle="modal" data-target="#exampleModalCenter" role="button">Read More <i class="fa fa-caret-right" aria-hidden="true"></i></a>
+								
+
+					</div>
+				</div>
+				<div class="carousel-item item2">
+					<div class="carousel-caption">
+						<h3>World wide transport for everyone</h3>
+						<p>Vinns Courier Services offer a diverse range of transportation services from project cargo to international transportation and domestic retail distribution and delivery.</p>
+							<a href="#" data-toggle="modal" data-target="#exampleModalCenter" role="button">Read More <i class="fa fa-caret-right" aria-hidden="true"></i></a>
+							
+
+					</div>
+				</div>
+				<div class="carousel-item item3">
+					<div class="carousel-caption">
+							<h3>24*7 services provided for Customers</h3>
+				<p>Vinns Courier Services offer a diverse range of transportation services from project cargo to international transportation and domestic retail distribution and delivery.</p>
+					<a href="#" data-toggle="modal" data-target="#exampleModalCenter" role="button">Read More <i class="fa fa-caret-right" aria-hidden="true"></i></a>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+		<!-- banner-bottom -->
+	<div class="banner-bottom">
+		<div class="container">
+			<div class="row">
+			<div class="col-lg-4 agileits_banner_bottom_left">
+				<div class="agileinfo_banner_bottom_pos">
+					<div class="w3_agileits_banner_bottom_pos_grid">
+						<div class="col-xs-3 wthree_banner_bottom_grid_left">
+							<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
+								<span class="fas fa-car" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="col-xs-9 wthree_banner_bottom_grid_right">
+							<h4>ROAD TRANSPORT</h4>
+							<p>Vinns Courier Services is a superior and expert express shipping company.</p>
+
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 agileits_banner_bottom_left">
+				<div class="agileinfo_banner_bottom_pos">
+					<div class="w3_agileits_banner_bottom_pos_grid">
+						<div class="col-xs-3 wthree_banner_bottom_grid_left">
+							<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
+								<span class="fas fa-fighter-jet" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="col-xs-9 wthree_banner_bottom_grid_right">
+							<h4>AIR FREIGHT</h4>
+							<p> Vinns Courier Services can handle all of your global forwarding /shipping / logistics solutions.</p>
+
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 agileits_banner_bottom_left">
+				<div class="agileinfo_banner_bottom_pos">
+					<div class="w3_agileits_banner_bottom_pos_grid">
+						<div class="col-xs-3 wthree_banner_bottom_grid_left">
+							<div class="agile_banner_bottom_grid_left_grid hvr-radial-out">
+								<span class="fas fa-subway" aria-hidden="true"></span>
+							</div>
+						</div>
+						<div class="col-xs-9 wthree_banner_bottom_grid_right">
+							<h4>RAILWAY LOGISTICS</h4>
+							<p>Step-by-step we provide the details needed to ensure expedient and profitable export transactions.</p>
+
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		</div>
+	</div>
+	<!-- //banner-bottom -->
+
+	</div>
+	</section>
+	<div class="container" style="width: 100%; height: 150px;">
+	</div>
+	<div class="container">
+	<div class="signin-form profile">
+				<h3 class="agileinfo_sign">Track Your Goods Here</h3>	
+						<div class="login-form">
+							<form action="#" method="post">
+							   <input type="text" name="good" placeholder="Enter Your Tracking Number Here" required="" style="color: white; background-color: black">
+								<input type="submit" name="track" value="Track">
+							</form>
+						</div>
+						<p><a href="#"> By clicking Track, I agree to your terms</a></p>
+					</div>
+</div>
+			<!-- main pricing table -->
+	<!-- /contact us -->
+	<div class="w3-contact py-5" id="contact">
+	<div class="container py-md-3">
+	<div class="w3-head-all  mb-5">
+				<h3>Contact us</h3>
+			</div>
+		<div class="w3-contact1">
+			<h1>100%</h1>
+			<h2>The Best Shipping Services</h2>
+			<p><a class="book popup-with-zoom-anim button-isi zoomIn animated" data-wow-delay=".5s" href="#small-dialog"><span class="fa fa-paper-plane-o"></span> Contact Us</a></p>
+		</div>
+		</div>
+	</div>
+		<!-- //contact us -->
+			<!-- map -->
+<div class="w3l-map">
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d10446319.224363243!2d-101.53641366492933!3d40.52127641868563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1509778660321" style="border:0" allowfullscreen></iframe>
+</div>
+<!-- Newsletter -->
+<section class="newsletter text-center py-5">
+	<div class="container py-md-3">
+		<h3 class="heading text-center text-uppercase mb-5">FOR BEST PRICE AND SERVICE </h3>
+		<div class="w3l_header_contact_details_agile">
+												<p>Give us a Call</p>
+												<a class="w3l_header_contact_details_agile-info_inner"> <span><i class="fas fa-phone"></i></span>7085733574</a>
+											</div>
+
+		<div class="subscribe_inner">
+			<p class="mb-4">Vinns Courier Services is a superior, express shipping company. Although the Company has been in practice for over 20 years, the founders offer in excess of 30 years valuable industry experience.</p>
+			<div class="social mt-sm-5 mt-3">
+				<h4>Follow us</h4>
+				<ul class="d-flex mt-3 justify-content-center">
+					<li class="mx-2"><a href="#"><span class="fab fa-facebook"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fab fa-twitter-square"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fas fa-rss-square"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fab fa-linkedin"></span></a></li>
+					<li class="mx-2"><a href="#"><span class="fab fa-google-plus-square"></span></a></li>
+				</ul>
+			</div>
+		</div>
+		
+	</div>
+</section>
+<footer>
+	<div class="container py-3 py-md-4">
+		<div class="footer">
+				<p class="text-center">© 2018 Transload. All Rights Reserved | Design by <a href="#" target="_blank">W3Layouts</a></p>
+			</div>
+	</div>
+</footer>
+<!-- footer -->
+
+<!-- //Newsletter -->
+<!-- /modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title text-uppercase" id="exampleModalLongTitle">Vinns Courier Services</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+		<img src="images/modal.jpg" class="img-fluid mb-4" alt="Modal Image" />
+        Vinns Courier Services is a superior, express shipping company. Although the Company has been in practice for over 20 years, the founders offer in excess of 30 years valuable industry experience. Our extensive knowledge of the express delivery market allows Vinns Courier Services to offer our clients an array of different shipping options from our unsurpassed worldwide and local distribution network.      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- //modal -->
+<!-- Modal2 -->
+<div class="modal fade" id="myModal3" tabindex="-1" role="dialog">
+	<div class="modal-dialog">
+	<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				
+				<div class="signin-form profile">
+				<h3 class="agileinfo_sign">Sign Up</h3>	
+						<div class="login-form">
+							<form action="#" method="post">
+							   <input type="text" name="name" placeholder="Username" required="">
+								<input type="email" name="email" placeholder="Email" required="">
+								<input type="password" name="password" placeholder="Password" required="">
+								<input type="password" name="password" placeholder="Confirm Password" required="">
+								<input type="submit" value="Sign Up">
+							</form>
+						</div>
+						<p><a href="#"> By clicking register, I agree to your terms</a></p>
+					</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- //Modal2 -->	
+
+<!-- /magnific-popup -->
+<div class="pop-up"> 
+				<div id="small-dialog" class="mfp-hide book-form">
+					<h3 class="sub-head-w3-agileits subscribe_us text-center py-3 head_size_con">Contact us </h3>
+						<!-- banner --> 
+	<div class="video" data-vide-bg="video/ship"> 
+	<div class="center-container">
+		 <div class="bg-agile">
+			<h3>Order Contact</h3>
+			<div class="login-form">			
+							<form action="#" method="post">
+								<input type="text"  name="Name" placeholder="Full name" required="" />
+								<input type="text"  name="Number" placeholder="Phone number" required="" />
+								<input type="email"  name="Email" placeholder="Email" required="" />
+								<h3>Shipping address</h3>
+								<div class="left-w3-agile">
+									<select class="form-control form2-color">
+										<option>Choose your country</option>
+										<option>Argentina</option>
+										<option>Georgia</option>
+										<option> Dominica</option>
+										<option>Lithuania</option>
+										<option> Monaco</option>
+									</select>
+									<input type="text"  name="Region" placeholder="State/province/region" required="" />
+								</div>
+								<div class="right-agileits">
+								<input type="text"  name="City" placeholder="City" required="" />
+								<input type="text"  name="Zip/postal code" placeholder="Zip/postal code" required="" />
+								</div>
+								<textarea name="message" value="Address" placeholder="Street address" ></textarea>
+								<input type="submit" value="Submit">
+							</form>	
+						</div>	
+		</div>
+	<!-- //banner --> 
+
+				</div>
+			</div>
+</div>
+</div>
+<!-- //magnific-popup -->
+
+<!-- js -->
+	<script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+	<!-- //js -->
+
+
+<!-- stats -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.countup.js"></script>
+	<script>
+		$('.counter').countUp();
+	</script>
+	<!-- //stats -->
+<!-- bars.js -->   
+	<script src="js/bars.js"></script>
+	<!-- //bars.js -->
+
+	<!-- flexSlider (for testimonials) -->
+	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+	<script defer src="js/jquery.flexslider.js"></script>
+	<script>
+		$(window).load(function () {
+			$('.flexslider').flexslider({
+				animation: "slide",
+				start: function (slider) {
+					$('body').removeClass('loading');
+				}
+			});
+		});
+	</script>
+	<!-- //flexSlider (for testimonials) -->
+	<!-- simpleLightbox -->
+	<script src="js/simpleLightbox.js"></script>
+	<script>
+		$('.proj_gallery_grid a').simpleLightbox();
+	</script>
+	<!-- //simpleLightbox -->
+<!--popup-js-->
+<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+ <script>
+						$(document).ready(function() {
+						$('.popup-with-zoom-anim').magnificPopup({
+							type: 'inline',
+							fixedContentPos: false,
+							fixedBgPos: true,
+							overflowY: 'auto',
+							closeBtnInside: true,
+							preloader: false,
+							midClick: true,
+							removalDelay: 300,
+							mainClass: 'my-mfp-zoom-in'
+						});
+																						
+						});
+</script>
+<!--//popup-js-->
+<!-- start-smooth-scrolling -->
+	<script src="js/move-top.js"></script>
+	<script src="js/easing.js"></script>
+	<script>
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000);
+			});
+		});
+	</script>
+	<!-- //end-smooth-scrolling -->
+	
+	<!-- smooth-scrolling-of-move-up -->
+	<script>
+		$(document).ready(function () {
+
+			$().UItoTop({
+				easingType: 'easeOutQuart'
+			});
+
+		});
+	</script>
+	<!-- //smooth-scrolling-of-move-up -->
+	
+	
+	<!-- smooth scrolling js -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<!-- //smooth scrolling js -->
+ </body>
+</html>
